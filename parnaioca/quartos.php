0c918a64810ec7a5e846498b2b387598ffb9edb0
Gabriel-Dames-Peixoto <dames.dames2018@gmail.com>
2026-02-13 10:06:41 -0300

    Subindo pasta parnaioca

---
+++ parnaioca/quartos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="1.css">
+    <link rel="shortcut icon" href="../imagens/ipousada.png" type="image/x-icon">
+    <title>Pousada Parnoica</title>
+</head>
+<body>
+
+
+</body>
+</html>
